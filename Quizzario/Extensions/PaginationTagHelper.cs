3fa033186f5a586c759739618210e5a526e918ba
Pawel Zoltaniecki <pawel.zoltaniecki@gmail.com>
2018-03-18 03:16:58 +0100

    Changes in Quizzario; Adding Paging Service/Functionality Add Pagitnation Tag Helper Add Paging Info and Paging Service

---
+++ Quizzario/Extensions/PaginationTagHelper.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Quizzario.Models;
+
+namespace Quizzario.Extensions
+{
+    public class PaginationTagHelper : TagHelper
+    {
+        public PagingInfo Info { get; set; }
+        public string PreviousPageText { get; set; } = "Previous";
+        public string NextPageText { get; set; } = "Next";        
+        public string Route { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            BuildParent(output);
+            if (Info.PreviousPage.Display)
+                AddPreviousPage(output);
+
+            AddPageNodes(output);
+
+            if (Info.NextPage.Display)
+                AddNextPage(output);
+        }
+
+        private static void BuildParent(TagHelperOutput output)
+        {
+            output.TagName = "ul";
+            output.Attributes.Add("class", "pagination");
+            output.Attributes.Add("role", "navigation");
+            output.Attributes.Add("aria-label", "Pagination");
+        }
+        
+        private void AddPreviousPage(TagHelperOutput output)
+        {
+            var html =
+$@"<li class=""pagination-previous"">
+    <a href=""{Route}/{Info.PreviousPage.PageNumber}"" aria-label=""{PreviousPageText} page"">{PreviousPageText} <span class=""show-for-sr"">page</span></a>
+</li>";
+
+            output.Content.SetHtmlContent(output.Content.GetContent() + html);
+        }
+        
+        private void AddNextPage(TagHelperOutput output)
+        {
+            var html =
+$@"<li class=""pagination-next"">
+    <a href=""{Route}/{Info.NextPage.PageNumber}"" aria-label=""{NextPageText} page"">{NextPageText} <span class=""show-for-sr"">page</span></a>
+</li>";
+
+            output.Content.SetHtmlContent(output.Content.GetContent() + html);
+        }
+
+        private void AddPageNodes(TagHelperOutput output)
+        {
+            foreach (var infoPage in Info.Pages)
+            {
+                string html;
+                if (infoPage.IsCurrent)
+                {
+                    html = $@"<li class=""current""><span class=""show-for-sr"">You're on page</span> {infoPage.PageNumber}</li>";
+                    output.Content.SetHtmlContent(output.Content.GetContent() + html);
+                    continue;
+                }
+                html = $@"<li><a href=""{Route}/{infoPage.PageNumber}"" aria-label=""Page {infoPage.PageNumber}"">{infoPage.PageNumber}</a></li>";
+                output.Content.SetHtmlContent(output.Content.GetContent() + html);
+            }
+        }
+    }
+}
+
---
+++ Quizzario/Services/PagingInfoService.cs
@@ -0,0 +1,188 @@
+﻿using Quizzario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quizzario.Services
+{
+    public interface IPaginatedMetaService
+    {
+        PagingInfo GetMetaData(int collectionSize, int selectedPageNumber, int itemsPerPage);
+    }
+
+    public class PagingInfoService
+    {        
+        private const int NumberOfNodesInPaginatedList = 9;
+        private List<Page> pages;
+
+        public PagingInfo GetMetaData(int collectionSize, int selectedPageNumber, int itemsPerPage)
+        {
+            if (collectionSize == 0)
+            {
+                return GetCollectionSizeZeroModel();
+            }
+
+            pages = BuildPageNodes(collectionSize, selectedPageNumber, itemsPerPage);
+            return new PagingInfo
+            {
+                PreviousPage = BuildPreviousPage(collectionSize, selectedPageNumber, itemsPerPage),
+                Pages = pages,
+                NextPage = BuildNextPage(collectionSize, selectedPageNumber, itemsPerPage)
+            };
+        }
+        
+        private static PagingInfo GetCollectionSizeZeroModel()
+        {
+            return new PagingInfo
+            {
+                PreviousPage = new PreviousPage
+                {
+                    Display = false
+                },
+                Pages = new List<Page>(),
+                NextPage = new NextPage
+                {
+                    Display = false
+                }
+            };
+        }
+
+        private PreviousPage BuildPreviousPage(int collectionSize, int selectedPageNumber, int itemsPerPage)
+        {
+            var display = selectedPageNumber > 1 && collectionSize >= itemsPerPage;
+            return new PreviousPage
+            {
+                Display = display,
+                PageNumber = display ? pages.First(x => x.IsCurrent).PageNumber - 1 : 1
+            };
+        }     
+
+        private NextPage BuildNextPage(int collectionSize, int selectedPageNumber, int itemsPerPage)
+        {
+            var display = selectedPageNumber < GetLastPageInCollection(collectionSize, itemsPerPage);
+            return new NextPage
+            {
+                Display = display,
+                PageNumber = display ? pages.First(x => x.IsCurrent).PageNumber + 1 : NumberOfNodesInPaginatedList + 1
+            };
+        }
+        
+        #region Page Nodes
+
+        private List<Page> BuildPageNodes(int collectionSize, int selectedPageNumber, int itemsPerPage)
+        {
+            var lastPage = GetLastPageInCollection(collectionSize, itemsPerPage);
+            //Partial List is not full list
+            if (NumberOfNodesInPaginatedList * itemsPerPage > collectionSize)
+            {
+                return BuildPartialList(collectionSize, selectedPageNumber, itemsPerPage);
+            }
+            int half = NumberOfNodesInPaginatedList / 2;
+            //Full list which start at page1
+            if (selectedPageNumber <= NumberOfNodesInPaginatedList - half)
+            {
+                return BuildStartList(selectedPageNumber);
+            }
+            //Full list which end at page{lastPage}
+            if (selectedPageNumber > lastPage - (NumberOfNodesInPaginatedList - half))
+            {
+                return BuildEndList(collectionSize, selectedPageNumber, itemsPerPage);
+            }
+            // We are at an in between collection of node in paginated list
+            return BuildFullList(selectedPageNumber);
+        }
+
+        /// <summary>
+        /// Build a full (NumberOfNodesInPaginatedList) collection of page nodes
+        /// [ ][ ][ ][x][x][x][x][x][x][x][x][x][x][ ][ ][ ]
+        /// </summary>
+        private List<Page> BuildFullList(int selectedPageNumber)
+        {
+            var pages = new List<Page>();
+            int half = NumberOfNodesInPaginatedList / 2;
+            for (int i = NumberOfNodesInPaginatedList - 1; i >= 0; i--)
+            {
+                pages.Add(BuildNode(selectedPageNumber + half - i));
+            };
+            int index = NumberOfNodesInPaginatedList % 2 == 0 ?
+                (NumberOfNodesInPaginatedList / 2) - 1 : NumberOfNodesInPaginatedList / 2;
+            pages[index].IsCurrent = true;
+
+            return pages;
+        }
+
+        /// <summary>
+        /// Build a full (NumberOfNodesInPaginatedList) collection of page nodes
+        /// [x][x][ ][ ][ ][ ][ ][ ][ ][ ] ][ ][ ][ ][ ][ ]
+        /// </summary>
+        private List<Page> BuildPartialList(int collectionSize, int selectedPageNumber, int itemsPerPage)
+        {
+            var pages = new List<Page>();
+            for (var i = 0; i < GetLastPageInCollection(collectionSize, itemsPerPage); i++)
+            {
+                pages.Add(BuildNode(i + 1));
+            }
+
+            pages[selectedPageNumber - 1].IsCurrent = true;
+
+            return pages;
+        }
+
+        /// <summary>
+        /// Build list when selected page falls into first collection of node list
+        /// Start shifting after three
+        /// [*][*][*][x][x][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]
+        /// </summary>
+        private List<Page> BuildStartList(int selectedPageNumber)
+        {
+            var pages = new List<Page>();
+            for (int i = 0; i < NumberOfNodesInPaginatedList; i++)
+            {
+                pages.Add(BuildNode(i + 1));
+            }
+            pages[selectedPageNumber - 1].IsCurrent = true;
+            return pages;
+        }
+
+        /// <summary>
+        /// Build list when selected page falls into last collection of nodes
+        /// Stop shifting after three from end
+        /// [ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][x][x][*][*][*]
+        /// </summary>
+        private List<Page> BuildEndList(int collectionSize, int selectedPageNumber, int itemsPerPage)
+        {
+            var lastPage = GetLastPageInCollection(collectionSize, itemsPerPage);
+
+            var pages = new List<Page>();
+            for (int i = NumberOfNodesInPaginatedList - 1; i >= 0; i--)
+            {
+                pages.Add(BuildNode(lastPage - i));
+            }
+
+            var unshiftedIndex = lastPage - selectedPageNumber;
+            pages[NumberOfNodesInPaginatedList - 1 - unshiftedIndex].IsCurrent = true;
+
+            return pages;
+        }
+
+        private Page BuildNode(int pageNumber)
+        {
+            return new Page
+            {
+                IsCurrent = false,
+                PageNumber = pageNumber
+            };
+        }
+
+        #endregion
+
+        private int GetLastPageInCollection(int collectionSize, int itemsPerPage)
+        {
+            var lastPage = (double)collectionSize / itemsPerPage;
+            if (lastPage % 1 == 0)
+                return (int)lastPage;
+            return (int)lastPage + 1;
+        }
+    }
+}
