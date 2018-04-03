ac01c52721d3c4cf885d4314eb05808a5575a4af
Michał Polak <polak.micha@gmail.com>
2018-04-03 23:03:15 +0200

    Added sidemenu
    Updated menu, created a little messy solution to make topmenu and sidemenu independent. Design is responsive and collapses properly on smaller viewports.

---
+++ raw views/js/custom.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('#-sidemenu-collapse-button').click(function() {	
+		$('.-sidemenu-label').toggle();
+		$('#-sidemenu').toggleClass('-sidemenu-collapsed');
+		$('#-content').toggleClass('-content-sidemenu-collapsed');
+		
+		$('#-sidemenu-collapse-left').toggleClass('d-none');
+		$('#-sidemenu-collapse-right').toggleClass('d-none');
+	});
+});
