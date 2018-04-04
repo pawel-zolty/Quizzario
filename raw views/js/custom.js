861b5b54642942b6d7fa87008017b32a4647b1e0
Michał Polak <polak.micha@gmail.com>
2018-04-04 19:41:44 +0200

    Basic refactoring, creating basic templates

--- raw views/js/custom.js
+++ raw views/js/custom.js
@@ -2,7 +2,7 @@ $(function() {
 	$('#-sidemenu-collapse-button').click(function() {	
 		$('.-sidemenu-label').toggle();
 		$('#-sidemenu').toggleClass('-sidemenu-collapsed');
-		$('#-content').toggleClass('-content-sidemenu-collapsed');
+		$('#-content-wrapper').toggleClass('-content-sidemenu-collapsed');
 		
 		$('#-sidemenu-collapse-left').toggleClass('d-none');
 		$('#-sidemenu-collapse-right').toggleClass('d-none');
