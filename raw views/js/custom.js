555313ba24ff14f593d143c53915d97f9a0115f8
Michał Polak <polak.micha@gmail.com>
2018-04-05 00:59:37 +0200

    Added my quizes raw view

--- raw views/js/custom.js
+++ raw views/js/custom.js
@@ -1,10 +1,17 @@
 $(function() {
+	// Sidemenu collapse button
 	$('#-sidemenu-collapse-button').click(function() {	
-		$('.-sidemenu-label').toggle();
+		$('#-sidemenu ul span').toggle();
 		$('#-sidemenu').toggleClass('-sidemenu-collapsed');
 		$('#-content-wrapper').toggleClass('-content-sidemenu-collapsed');
 		
 		$('#-sidemenu-collapse-left').toggleClass('d-none');
 		$('#-sidemenu-collapse-right').toggleClass('d-none');
+	});
+	
+	// Quizes list on click
+	$('.-quizes-card').click(function() {
+		$('.-quizes-card-active').toggleClass('-quizes-card-active');
+		$(this).toggleClass('-quizes-card-active');
 	});
 });
