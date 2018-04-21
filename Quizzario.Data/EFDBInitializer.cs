97941123f94bc193a3530bd9185675ca60148de9
Binqq <m-michal-b@wp.pl>
2018-04-21 15:41:33 +0200

    corrected errors

--- Quizzario.Data/EFDBInitializer.cs
+++ Quizzario.Data/EFDBInitializer.cs
@@ -30,31 +30,31 @@ namespace Quizzario.Data
 
             var quizes = new List<Quiz>
             {
-            new Quiz{Id = 1, Title = "User's 1 Quiz", ApplicationUserId = 1, FilePath = "C://q1" },
-            new Quiz{Id = 2, Title = "User's 2 Quiz", ApplicationUserId = 2, FilePath = "C://q2", QuizType = QuizType.Quiz}
+            new Quiz{Id = "1", Title = "User's 1 Quiz", ApplicationUserId = "1", FilePath = "C://q1" },
+            new Quiz{Id = "2", Title = "User's 2 Quiz", ApplicationUserId = "2", FilePath = "C://q2", QuizType = QuizType.Quiz}
             };
             quizes.ForEach(q => context.Quizes.Add(q));
             context.SaveChanges();
 
             var assignedUser = new List<AssignedUser>
             {
-            new AssignedUser{QuizId = 1, ApplicationUserId = 2, AssignType = AssignType.Favourite},
-            new AssignedUser{QuizId = 1, ApplicationUserId = 2, },
+            new AssignedUser{QuizId = "1", ApplicationUserId = "2", AssignType = AssignType.Favourite},
+            new AssignedUser{QuizId = "1", ApplicationUserId = "2", },
             };
             assignedUser.ForEach(au => context.AssignedUsers.Add(au));
             context.SaveChanges();
 
             var scores = new List<Score>
             {
-            new Score{QuizId = 1, ApplicationUserId = 1, Result = 10},
-            new Score{QuizId = 1, ApplicationUserId = 2, Result = 5}
+            new Score{QuizId = "1", ApplicationUserId = "1", Result = 10},
+            new Score{QuizId = "1", ApplicationUserId = "2", Result = 5}
             };
             scores.ForEach(au => context.Scores.Add(au));
             context.SaveChanges();
 
             var settings = new List<QuizSettings>
             {
-            new QuizSettings{QuizId = 1, AttemptLimit = 3}
+            new QuizSettings{QuizId = "1", AttemptLimit = 3}
             };
             settings.ForEach(s => context.QuizSettingss.Add(s));
             context.SaveChanges();
