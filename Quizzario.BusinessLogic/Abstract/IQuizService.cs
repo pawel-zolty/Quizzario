101bba6886e11e4c38a86ea654c1084ace32eb43
Pawel Zoltaniecki <pawel.zoltaniecki@gmail.com>
2018-04-23 17:39:31 +0200

    Create Interface IQuizService Create QuizService

---
+++ Quizzario.BusinessLogic/Abstract/IQuizService.cs
@@ -0,0 +1,15 @@
+﻿using Quizzario.BusinessLogic.Models;
+using Quizzario.Data.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzario.BusinessLogic.Abstract
+{
+    public interface IQuizService
+    {
+        IEnumerable<QuizDTO> GetAllQuizes();
+    }
+}
---
+++ Quizzario.BusinessLogic/Services/QuizService.cs
@@ -0,0 +1,28 @@
+﻿using Quizzario.BusinessLogic.Abstract;
+using Quizzario.BusinessLogic.Models;
+using Quizzario.Data.Abstracts;
+using Quizzario.Data.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzario.BusinessLogic.Services
+{
+    public class QuizService : IQuizService
+    {
+        private IQuizDTOFactory factory;
+
+        public QuizService(IQuizDTOFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public IEnumerable<QuizDTO> GetAllQuizes()
+        {         
+            IEnumerable<QuizDTO> quizes = factory.CreateAllQuizes();
+            return quizes;
+        }
+    }
+}
