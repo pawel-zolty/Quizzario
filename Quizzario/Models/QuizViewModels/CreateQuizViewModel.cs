0a57834ab355bb638201aa5d197f49552b9a359f
Jakub Krzemiński <krzeminski.k@outlook.com>
2018-05-08 20:32:53 +0200

    Quiz creation view model

---
+++ Quizzario/Models/QuizViewModels/CreateQuizViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quizzario.Models.QuizViewModels
+{
+    public class CreateQuizViewModel
+    {
+        public CreateQuizViewModel()
+        {
+            Questions = new List<CreateQuestionViewModel>();
+        }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<CreateQuestionViewModel> Questions { get; set; }
+    }
+    public class CreateQuestionViewModel
+    {
+        public string Question { get; set; }
+        public List<String> Answers { get; set; }
+        public string CorrectAnswer { get; set; }
+        public int QuestionNumber { get; set; }
+    }
+}
