5def22f4fe9827ef7b9b5508d029a915bc3f4ccf
Binqq <m-michal-b@wp.pl>
2018-06-02 16:39:22 +0200

    a

---
+++ Quizzario/Models/QuizViewModels/CreateQuizViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quizzario.Models.QuizViewModels
+{
+    public class CreateQuizViewModel
+    {
+        public CreateQuizViewModel()
+        {
+            Questions = new List<CreateQuestionViewModel>();
+        }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<CreateQuestionViewModel> Questions { get; set; }
+    }
+    public class CreateQuestionViewModel
+    {
+        public CreateQuestionViewModel()
+        {
+            Answers = new List<CreateAnswerViewModel>();
+        }
+        public string Question { get; set; }
+        public List<CreateAnswerViewModel> Answers { get; set; }
+    }
+    public class CreateAnswerViewModel
+    {
+        public string Answer { get; set; }
+        public bool isCorrect { get; set; }
+
+        public CreateAnswerViewModel()
+        {
+            isCorrect = false;
+        }
+
+    }
+}
