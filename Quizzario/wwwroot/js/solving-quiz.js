ca0b3e0a4b88fc76e00f1a16ec426f9b96cbd1f7
Michał Polak <polak.micha@gmail.com>
2018-06-08 18:23:30 +0200

    Added pending requests aborting

--- Quizzario/wwwroot/js/solving-quiz.js
+++ Quizzario/wwwroot/js/solving-quiz.js
@@ -1,22 +1,29 @@
 ﻿$(function () {
+    var solvingGetQuestionXHR;
+    var solvingGetQuestionActive = false;
+
     $('.-taking-quiz-button').click(function () {
         var content = $("#-taking-quiz-right-panel-content");
         var parent = content.parent();
         var heightOfParent = parent.height();
         parent.height(heightOfParent);
 
-        content.hide();
+        content.html("<h5>Loading...</h5>");
 
         var number = $(this).data('number');
-        $.post("/Quizes/SolvingGetQuestion", {
+
+        if (solvingGetQuestionActive) solvingGetQuestionXHR.abort();       // Abort pending ajax requests
+        solvingGetQuestionActive = true;
+        solvingGetQuestionXHR = $.post("/Quizes/SolvingGetQuestion", {
             number: number
         }).done(function (res) {
+            content.hide();
             content.html(res);
-        }).fail(function (res) {
-            content.html("dupa");
         }).always(function () {
             content.fadeIn(500);
             parent.height("");
+
+            solvingGetQuestionActive = false;
         });
     });
 });
